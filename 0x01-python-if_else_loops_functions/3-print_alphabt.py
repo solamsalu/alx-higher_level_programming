39a76a7aa4f2f56abaa7a5601da7acff98b3b0e2
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-03-07 21:24:59 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
+for alpha_letter in range(ord('a'), ord('z')+1):
     if alpha_letter != ord('q') and alpha_letter != ord('e'):
         print('{:c}'.format(alpha_letter), end="")
