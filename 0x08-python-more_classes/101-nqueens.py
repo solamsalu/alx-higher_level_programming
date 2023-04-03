bf63e0ed6e1ac4012593a8bbaecb11562228486f
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-04-03 23:21:02 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,59 +1,81 @@
 #!/usr/bin/python3
-"""N Queens Module.
-Contains the N Queens problem solver.
-"""
-import sys
+"""Module is to solve the N-Queens challenge problem"""
+from sys import argv
 
 
-def error_exit(message="", code=1):
-    """Handles exit.
-    Args:
-        message (str): the message to display on stdout.
-        code (int): the exit code.
-    """
-    print(message)
-    exit(code)
+def checkspot(board, r, c):
+    n = len(board) - 1
+    if board[r][c]:
+        return 0
+    for row in range(r):
+        if board[row][c]:
+            return 0
+    i = r
+    j = c
+    while i > 0 and j > 0:
+        i -= 1
+        j -= 1
+        if board[i][j]:
+            return 0
+    i = r
+    j = c
+    while i > 0 and j < n:
+        i -= 1
+        j += 1
+        if board[i][j]:
+            return 0
+    return 1
 
 
-def test_pos(board, y):
-    """Tests if wether a queen can be placed at the current position.
-    Args:
-        board (list): the chessboard.
-        y (int): the height parameter.
-    """
-    for i in range(y):
-        if board[y][1] is board[i][1]:
-            return False
-        if abs(board[y][1] - board[i][1]) == y - i:
-            return False
-    return True
+def initboard(n=4):
+    b = []
+    for r in range(n):
+        b.append([0 for c in range(n)])
+    return b
 
 
-def rec_backtrack(board, y):
-    """Backtrack the possibilities.
-    Args:
-        board (list): the chessboard.
-        y (int): the height parameter.
-    """
-    if y is N:
-        print(board)
-    else:
-        for x in range(N):
-            board[y][1] = x
-            if test_pos(board, y):
-                rec_backtrack(board, y + 1)
+def findsoln(board, row):
+    for col in range(len(board)):
+        if checkspot(board, row, col):
+            board[row][col] = 1
+            if row == len(board) - 1:
+                print(convtosoln(board))
+                board[row][col] = 0
+                continue
+            if findsoln(board, row + 1):
+                return board
+            else:
+                board[row][col] = 0
+    return None
 
 
-if len(sys.argv) is not 2:
-    error_exit("Usage: nqueens N")
+def convtosoln(board):
+    soln = []
+    n = len(board)
+    for r in range(n):
+        for c in range(n):
+            if board[r][c]:
+                soln.append([r, c])
+    return soln
 
-try:
-    N = int(sys.argv[1])
-except:
-    error_exit("N must be a number")
 
-if N < 4:
-    error_exit("N must be at least 4")
+def nqueens(n=4):
+    for col in range(n):
+        board = initboard(n)
+        board[0][col] = 1
+        findsoln(board, 1)
 
-board = [[y, 0] for y in range(N)]
-rec_backtrack(board, 0)
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        n = int(argv[1])
+    except:
+        print("N must be a number")
+        exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    nqueens(n)
