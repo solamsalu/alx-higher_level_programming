e90119eacbbbdcbdd652a58f564e0c5e4862a565
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-04-17 22:26:13 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module that contains class Base. It defines a Base classes in the project. """
+""" Module that contains class Base. """
 import json
 import csv
 import os.path
