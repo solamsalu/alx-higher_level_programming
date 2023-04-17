219fc9763b0af1636f6c1153555f746313cc0f33
Solomon Amsalu <solamsalu@gmail.com>
2023-04-17 06:20:35 -0700

    update all

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" Module square.
+Create a Square class, inheriting from Rectangle.
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Class describing a square inherits form  Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes a Square instances """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Returns a string representation of a Square instance. """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
+
+    @property
+    def size(self):
+        """ Retrieves the size attribute """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Sets the  size attribute """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Returns a string representation of a Square instance """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
+
+    def update(self, *args, **kwargs):
+        """ update attributes of an instace """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns a dictionary representation of a Square. """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
