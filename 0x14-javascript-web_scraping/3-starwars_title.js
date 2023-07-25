b54f5c5528df1a72c9ffd7d87f255c6e967ea8fd
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-07-25 17:19:05 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 const req = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/${id}';
-request.get(url, (err, res, body) => {
-    if (err) {
-        console.error(err);
-    } else {
-        let data = JSON.parse(body);
-        if (data.title) {
-            console.log(data.title);
-        } else {
-            console.log("Invalid movie ID");
-        }
-    }
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
