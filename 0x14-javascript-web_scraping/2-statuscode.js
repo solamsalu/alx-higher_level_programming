09903c14ba0f535af385ff3fb16f34bb6bf09cce
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-07-25 17:16:12 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const url = process.argv[2];
-request(url, function (err, res) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('code: ' + res.statusCode);
-    }
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
