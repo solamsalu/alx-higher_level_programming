7285a98d3c366f9392c6052b0b65e86cd9681244
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-07-25 17:07:57 +0300

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require("request");
-request.get(process.argv[2], (err, res, body) => {
+request(process.argv[2], (err, res, body) => {
     if (err) {
         console.error(err);
     } else {
